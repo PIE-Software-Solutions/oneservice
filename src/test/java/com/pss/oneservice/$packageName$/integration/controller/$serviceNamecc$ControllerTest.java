4cf25faf09aaff042b5146b21c09e1af74cfd8bc
Kiran Barnana <admin@piesoftsol.com>
2019-08-10 22:28:24 +0530

    added test cases

---
+++ src/test/java/com/pss/oneservice/$packageName$/integration/controller/$serviceNamecc$ControllerTest.java
@@ -0,0 +1,74 @@
+package com.pss.oneservice.$packageName$.integration.controller;
+
+import static com.pss.oneservice.common.integration.util.CommonConstants.ONESERVICE_JDBC_TEMPLATE;
+import static org.mockito.Mockito.mock;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import com.pss.oneservice.$packageName$.integration.controller.$serviceNamecc$Controller;
+import com.pss.oneservice.$packageName$.integration.dao.$serviceNamecc$DAO;
+import com.pss.oneservice.$packageName$.integration.service.$serviceNamecc$Service;
+
+/**
+ * JUnit Test class for $serviceNamecc$ service
+ * 
+ * @author $author$
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class $serviceNamecc$ControllerTest {
+
+	@TestConfiguration
+	static class TestContextConfiguration {
+
+		static JdbcTemplate oneServiceJdbcTemplate = mock(JdbcTemplate.class);
+
+		@Bean
+		public $serviceNamecc$Controller $serviceName$Controller() {
+			return new $serviceNamecc$Controller();
+		}
+
+		@Bean
+		public $serviceNamecc$Service $serviceName$Service() {
+			return new $serviceNamecc$Service();
+		}
+
+		@Bean
+		public $serviceNamecc$DAO $serviceName$DAO() {
+			return new $serviceNamecc$DAO();
+		}
+
+		@Bean
+		@Qualifier(ONESERVICE_JDBC_TEMPLATE)
+		public JdbcTemplate oneServiceJdbcTemplate() {
+			return oneServiceJdbcTemplate;
+		}
+	}
+
+	@Autowired
+	private $serviceNamecc$Controller $serviceName$Controller;
+
+	// Success scenarios
+	@Test
+	public void testSuccessScenario() {
+		// TODO Success test cases
+	}
+
+	// Error scenarios
+	@Test
+	public void testErrorScenario() {
+		// TODO Error test cases
+	}
+}
