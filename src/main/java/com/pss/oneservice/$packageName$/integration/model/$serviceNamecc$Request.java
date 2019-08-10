61bf66547df485c612b545aabde2ada5e2c6d619
Kiran Barnana <admin@piesoftsol.com>
2019-08-11 00:07:33 +0530

    removed unwanter data

--- src/main/java/com/pss/oneservice/$packageName$/integration/model/$serviceNamecc$Request.java
+++ src/main/java/com/pss/oneservice/$packageName$/integration/model/$serviceNamecc$Request.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.pss.oneservice.common.integration.model.ServiceRequest;
 
@@ -18,69 +17,14 @@ import com.pss.oneservice.common.integration.model.ServiceRequest;
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
-    "JUR",
-    "WC"
+
 })
 public class $serviceNamecc$Request
     extends ServiceRequest
 {
 
-    /**
-     * 
-     * (Required)
-     * 
-     */
-    @JsonProperty("JUR")
-    private String jur;
-    /**
-     * 
-     * (Required)
-     * 
-     */
-    @JsonProperty("WC")
-    private String wc;
     @JsonIgnore
     private Map<String, Object> additionalProperties = new HashMap<String, Object>();
-
-    /**
-     * 
-     * (Required)
-     * 
-     */
-    @JsonProperty("JUR")
-    public String getJur() {
-        return jur;
-    }
-
-    /**
-     * 
-     * (Required)
-     * 
-     */
-    @JsonProperty("JUR")
-    public void setJur(String jur) {
-        this.jur = jur;
-    }
-
-    /**
-     * 
-     * (Required)
-     * 
-     */
-    @JsonProperty("WC")
-    public String getWc() {
-        return wc;
-    }
-
-    /**
-     * 
-     * (Required)
-     * 
-     */
-    @JsonProperty("WC")
-    public void setWc(String wc) {
-        this.wc = wc;
-    }
 
     @JsonAnyGetter
     public Map<String, Object> getAdditionalProperties() {
@@ -110,14 +54,6 @@ public class $serviceNamecc$Request
         if (sb.length()>baseLength) {
             sb.append(',');
         }
-        sb.append("jur");
-        sb.append('=');
-        sb.append(((this.jur == null)?"<null>":this.jur));
-        sb.append(',');
-        sb.append("wc");
-        sb.append('=');
-        sb.append(((this.wc == null)?"<null>":this.wc));
-        sb.append(',');
         sb.append("additionalProperties");
         sb.append('=');
         sb.append(((this.additionalProperties == null)?"<null>":this.additionalProperties));
@@ -134,8 +70,6 @@ public class $serviceNamecc$Request
     public int hashCode() {
         int result = 1;
         result = ((result* 31)+((this.additionalProperties == null)? 0 :this.additionalProperties.hashCode()));
-        result = ((result* 31)+((this.wc == null)? 0 :this.wc.hashCode()));
-        result = ((result* 31)+((this.jur == null)? 0 :this.jur.hashCode()));
         result = ((result* 31)+ super.hashCode());
         return result;
     }
@@ -149,7 +83,7 @@ public class $serviceNamecc$Request
             return false;
         }
         $serviceNamecc$Request rhs = (($serviceNamecc$Request) other);
-        return (((super.equals(rhs)&&((this.additionalProperties == rhs.additionalProperties)||((this.additionalProperties!= null)&&this.additionalProperties.equals(rhs.additionalProperties))))&&((this.wc == rhs.wc)||((this.wc!= null)&&this.wc.equals(rhs.wc))))&&((this.jur == rhs.jur)||((this.jur!= null)&&this.jur.equals(rhs.jur))));
+        return (super.equals(rhs)&&((this.additionalProperties == rhs.additionalProperties)||((this.additionalProperties!= null)&&this.additionalProperties.equals(rhs.additionalProperties))));
     }
 
 }
